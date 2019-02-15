b3e8874cd9fa963034fa0e793d5bf361f85f12a9
Li Xi <xl404@cam.ac.uk>
2019-02-15 17:34:57 +0000

    Changed to rebuild script

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Building hypervault/composer-rest-server-fork"
-source ./composer-rest-server/docker/build.sh
-
-echo "Building the hypervault/rest-server image"
-docker build -t hypervault/rest-server .
-
